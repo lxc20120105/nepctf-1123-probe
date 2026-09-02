7e4a50df78f0d34aec51f5b90f4b370f28ecb855
lxc20120105 <lxc20120105@users.noreply.github.com>
2026-09-02 17:45:24 +0800

    deferred dump: read previous pending buffer on each hit

--- bpcmd2.py
+++ bpcmd2.py
@@ -1,42 +1,35 @@
 import lldb
 import sys
 
-pending = {}
+pending = []
 
 
 def _reg(frame, name):
     return frame.FindRegister(name).GetValueAsUnsigned()
 
 
+def _dump(proc):
+    global pending
+    while pending:
+        addr, n = pending.pop(0)
+        err = lldb.SBError()
+        data = proc.ReadMemory(addr, n, err)
+        if err.Fail():
+            print("READ n=%d <unreadable>" % n)
+        else:
+            print("READ n=%d %s" % (n, bytes(data).hex()))
+        sys.stdout.flush()
+
+
 def on_read(frame, bp_loc, dict):
     fd = _reg(frame, "x0")
     buf = _reg(frame, "x1")
     n = _reg(frame, "x2")
-    if fd <= 2 or n > 64 or n == 0:
-        return False
-    lr = _reg(frame, "lr")
-    target = frame.GetThread().GetProcess().GetTarget()
-    bp = target.BreakpointCreateByAddress(lr)
-    bp.SetOneShot(True)
-    bp.SetScriptCallbackFunction("bpcmd2.on_read_done")
-    pending[lr] = (buf, n)
-    return False
-
-
-def on_read_done(frame, bp_loc, dict):
-    pc = frame.GetPC()
-    info = pending.pop(pc, None)
-    if info is None:
-        return False
-    addr, n = info
     proc = frame.GetThread().GetProcess()
-    err = lldb.SBError()
-    data = proc.ReadMemory(addr, n, err)
-    if err.Fail():
-        print("READ n=%d ERR" % n)
-    else:
-        print("READ n=%d %s" % (n, bytes(data).hex()))
-    sys.stdout.flush()
+    if proc is not None:
+        _dump(proc)
+    if fd > 2 and 0 < n <= 64:
+        pending.append((buf, n))
     return False
 
 
@@ -44,14 +37,23 @@ def on_write(frame, bp_loc, dict):
     fd = _reg(frame, "x0")
     buf = _reg(frame, "x1")
     n = _reg(frame, "x2")
-    if fd <= 2 or n > 64 or n == 0:
-        return False
     proc = frame.GetThread().GetProcess()
-    err = lldb.SBError()
-    data = proc.ReadMemory(buf, n, err)
-    if err.Fail():
-        print("WRITE n=%d ERR" % n)
-    else:
-        print("WRITE n=%d %s" % (n, bytes(data).hex()))
-    sys.stdout.flush()
+    if proc is not None:
+        _dump(proc)
+    if fd > 2 and 0 < n <= 64:
+        err = lldb.SBError()
+        data = proc.ReadMemory(buf, n, err)
+        if err.Fail():
+            print("WRITE n=%d <unreadable>" % n)
+        else:
+            print("WRITE n=%d %s" % (n, bytes(data).hex()))
+        sys.stdout.flush()
+    return False
+
+
+def on_stdout(frame, bp_loc, dict):
+    """hook write to fd 1/2 so we can flush the last pending reads"""
+    proc = frame.GetThread().GetProcess()
+    if proc is not None:
+        _dump(proc)
     return False
